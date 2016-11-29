94f55e071fe25e2487531d9c586e9ea183758dbd
Riccardo <riccardo.cortelazzo@hotmail.it>
2016-11-29 20:04:58 +0100

    Base Temaplate

---
+++ js/anim.js
@@ -0,0 +1,83 @@
+$.fn.scrollView = function () {
+    return this.each(function () {
+        $('html, body').animate({
+            scrollTop: $(this).offset().top
+        }, 500);
+    });
+}
+
+function is_touch_device() {
+  return 'ontouchstart' in window        // works on most browsers
+      || navigator.maxTouchPoints;       // works on IE10/11 and Surface
+};
+
+function scrollTo(target)
+{
+    $(target).scrollView();
+}
+
+function closeAllCollapsable()
+{
+  $(document.body).find('.collapse').each(function(index) { $(this).collapse('hide');})
+}
+
+//use only on navbar!
+function scrollToCollapsed(target, mTarget = "")
+{
+  $(target).collapse('show');
+  if(mTarget != "")
+    $(mTarget).scrollView();
+   else $(target).scrollView();
+  if($('.navbar-toggle').is(':visible'))
+    $('.navbar-toggle').click()
+}
+
+function scrollToCollapsedToggle(target)
+{
+  $(target).collapse('toggle');
+  if(!$(target).hasClass('in'))
+    {$(target).scrollView();
+    }
+}
+
+function scrollHandler()
+{
+  var arrow = $('#backToTop');
+  if($(document).scrollTop()>200)
+      arrow.fadeIn();
+   else arrow.fadeOut();
+}
+
+function overlayAnimation()
+{
+  var $overlay = $('#img-over');
+  $overlay.css('top', '-'+$overlay.css('height'));
+  $overlay.fadeIn(100);
+  $overlay.animate({top:'0px'},'normal');
+}
+
+function OpenFullScreenImage(sender)
+{
+  //prepare overlay
+  var overlay = document.getElementById('img-over');
+  overlay.getElementsByTagName('img')[0].src = sender.getElementsByTagName('img')[0].src;
+  overlay.getElementsByClassName("o-title")[0].innerHTML = sender.getElementsByClassName("pic-title")[0].innerHTML;
+  overlay.getElementsByClassName("o-desc")[0].innerHTML = sender.getElementsByClassName("pic-desc")[0].innerHTML;
+
+  var $overlay = $('#img-over');
+  //animate
+  $overlay.css('top', '-'+$overlay.css('height'));
+  $overlay.fadeIn(100);
+  $overlay.animate({top:'0px'},'normal');
+  document.body.style.overflow = "hidden";
+}
+
+function closeFullScreenImage()
+{
+  var $overlay = $('#img-over');
+  $overlay.animate({top:'-'+$overlay.css('height')},'normal');
+  $overlay.fadeOut(100);
+  document.body.style.overflow = "visible";
+}
+
+window.onscroll = scrollHandler;
---
+++ js/main.js
@@ -0,0 +1,49 @@
+var AImagesToResize = [];
+
+function is_touch_device() {
+  return 'ontouchstart' in window        // works on most browsers
+      || navigator.maxTouchPoints;       // works on IE10/11 and Surface
+};
+
+function removeHover(sender)
+{
+  if(!is_touch_device()) return;
+
+  var title = sender.getElementsByClassName("pic-title")[0];
+  var desc = sender.getElementsByClassName("pic-desc")[0];
+
+  //inverti titolo e descrizion
+  var string = desc.innerHTML;
+  desc.innerHTML = title.innerHTML;
+  title.innerHTML = string;
+
+  desc.className = "pic-title pic-desc-s";
+  title.className = "pic-desc pic-title-s";
+  title.style.display = "none";
+  sender.className = "col-xs-12 col-sm-6 col-md-3 nopadding nooverflow";
+}
+
+function centerImg(target)
+{
+  target.style.position= "relative";
+  target.style.left= -target.width/2 +target.parentElement.clientWidth/2 +"px";
+}
+
+function ResizeImage(sender)
+{
+  AImagesToResize.push(sender);
+  centerImg(sender);
+  removeHover(sender.parentElement);
+}
+
+function centerAllImages()
+{
+  var i;
+  for(i=0;i<AImagesToResize.length;i++)
+  {
+    centerImg(AImagesToResize[i]);
+  }
+}
+
+
+window.onresize = centerAllImages;
